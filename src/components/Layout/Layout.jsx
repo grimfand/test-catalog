b9664a5bf3ebcb7fd39dbd401f1b65baddb81bf9
Valeriy Stepashin <val.stepashin@gmail.com>
2025-05-06 11:42:28 +0300

    chore: Layout init

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-export default function Layout(children) {
+export default function Layout({ children }) {
 	return (
 		<>
 			<Header />
-			{children}
+			<main>{children}</main>
 			<Footer />
 		</>
 	)
