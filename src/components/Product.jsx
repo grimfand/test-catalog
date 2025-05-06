f773e285ba68637e8f051c7c8ea7b434879a7bd3
Valeriy Stepashin <val.stepashin@gmail.com>
2025-05-06 16:02:15 +0300

    fix: date, rating

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,17 @@ import styles from './Product.module.css'
 import Button from './UI/button/Button'
 
 export default function Product(props) {
+	const createdAt = new Date(props.product.createdAt)
+	const fixedDateFormat = createdAt.toLocaleDateString('en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	})
+
+	const rating = Math.round(props.product.rating / 10)
+
 	return (
 		<div className={styles.product}>
 			<h2>{props.product.name}</h2>
@@ -13,8 +24,8 @@ export default function Product(props) {
 			/>
 			<p>{props.product.description}</p>
 			<span>Price: {props.product.price}</span>
-			<span>Rating: {props.product.rating}</span>
-			<span>{props.product.createdAt}</span>
+			<span>Rating: {rating}</span>
+			<span>Created at: {fixedDateFormat}</span>
 			<Button onClick={() => props.remove(props.product)}>Delete</Button>
 		</div>
 	)
