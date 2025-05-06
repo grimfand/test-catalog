03663c77cd5403a69d3231fe5e039ed6793a8ea9
Valeriy Stepashin <val.stepashin@gmail.com>
2025-05-06 12:28:38 +0300

    chore: add ProductList and Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import Layout from './components/Layout/Layout'
+import React, { useState } from 'react'
+import ProductList from './components/ProductList'
+import ProductForm from './components/ProductForm'
 
-function App() {
-	return <Layout>1</Layout>
+export default function App() {
+	const [products, setProducts] = useState([
+		{ id: 1, name: 'Name', description: 'Descr', price: 500 },
+		{ id: 2, name: 'Name', description: 'Descr', price: 500 },
+		{ id: 3, name: 'Name', description: 'Descr', price: 500 },
+	])
+
+	// async function fetchProducts() {
+	// 	let response = await fetch(
+	// 		'https://6818644b5a4b07b9d1ceddd8.mockapi.io/api/v1/products'
+	// 	)
+	// 	const data = await response.json()
+	// 	console.log(data)
+	// 	return data
+	// }
+
+	function addProduct(product) {
+		setProducts([...products, product])
+	}
+
+	return (
+		<>
+			{/* <button onClick={fetchProducts}>Load products</button> */}
+			<h1>Catalog:</h1>
+			<ProductList products={products} />
+
+			<h2>Add new Product:</h2>
+			<ProductForm add={addProduct} />
+		</>
+	)
 }
-
-export default App
