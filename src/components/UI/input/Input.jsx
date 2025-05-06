6c1012bd42cc3f3449585415b296e575097c9315
Valeriy Stepashin <val.stepashin@gmail.com>
2025-05-06 12:27:10 +0300

    fix: props

--- src/components/UI/input/Input.jsx
+++ src/components/UI/input/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Input.module.css'
 
-export default function Input(...props) {
+export default function Input({ ...props }) {
 	return <input className={styles.input} {...props} />
 }
