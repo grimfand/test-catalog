c64b8b85a77cd8a37d145643c37c76b815e31164
Valeriy Stepashin <val.stepashin@gmail.com>
2025-05-06 15:46:13 +0300

    chore: add no products message

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,10 +4,15 @@ import styles from './ProductList.module.css'
 
 export default function ProductList({ products, remove }) {
 	return (
-		<div className={styles.productList}>
-			{products.map(product => (
-				<Product product={product} key={product.id} remove={remove} />
-			))}
-		</div>
+		<>
+			{products.length === 0 && (
+				<p style={{ textAlign: 'center' }}>Products not found</p>
+			)}
+			<div className={styles.productList}>
+				{products.map(product => (
+					<Product product={product} key={product.id} remove={remove} />
+				))}
+			</div>
+		</>
 	)
 }
